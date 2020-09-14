2261991882804202c2e88ae9e78aed9494976230
Alberto Marin Estremadoiro <a2019116197@estudiantes.upsa.edu.bo>
2020-09-13 22:19:25 -0400

    API creado, UnitTest hecho, JWT aplicado

---
+++ Pregunta1/UnitTestPregunta1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPregunta1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGet()
+        {
+            //ARRANGE
+            MarinsController controller = new MarinsController();
+            //ACT
+            //ASSERT
+        }
+        [TestMethod]
+        public void TestPost()
+        {
+        }
+        [TestMethod]
+        public void TestPut()
+        {
+        }
+        [TestMethod]
+        public void TestDelete()
+        {
+        }
+    }
+}
